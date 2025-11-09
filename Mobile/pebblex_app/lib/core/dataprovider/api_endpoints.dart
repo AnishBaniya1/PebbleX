1f493a06211a6f570f8e20fbd5170a0ad5da00c2
Anish Baniya <anishbaniya07@gmail.com>
2025-11-09 15:25:52 +0545

    feat: integrate flutter_secure_storage for secure data handling

---
+++ Mobile/pebblex_app/lib/core/dataprovider/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  static const String storeKey = '';
+  static const String _devUrl = '';
+  // static const String _prodUrl = '';
+  static const String baseUrl = _devUrl;
+
+  static const String postLoginApi = '$baseUrl/token';
+}
